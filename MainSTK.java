2cdf3dc9e71285f8237c16deb0ee297595559a95
Aayush Koul <141576491+jellybean1402@users.noreply.github.com>
2024-02-16 20:13:39 +0530

    Update MainSTK.java

--- MainSTK.java
+++ MainSTK.java
@@ -10,7 +10,19 @@ public class MainSTK
 		int stack[] = new int[5];
 		int t = -1;
 		ArrayList<Integer> stackArray = new ArrayList<Integer>();
-		//Interface_STK fixed_stk = new Fixed_stk(stack, t);
+		Interface_STK fixed_stk = new Fixed_stk(stack, t);
+		
+		System.out.println("Fixed Stack");
+		
+		fixed_stk.push(1);
+		fixed_stk.push(2);
+		fixed_stk.push(3);
+		fixed_stk.push(4);
+		fixed_stk.displayStack();
+		fixed_stk.pop();
+		fixed_stk.displayStack();
+		
+		System.out.println("Growable Stack: ");
 		
 		Growable_stk grow_stk = new Growable_stk();
 		
@@ -20,6 +32,9 @@ public class MainSTK
 		grow_stk.push(4);
 		grow_stk.push(5);
 		grow_stk.push(6);
+		grow_stk.displayStack();
+		System.out.println();
+		grow_stk.pop();
 		grow_stk.displayStack();
 	}
 	
